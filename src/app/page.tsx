38babb75acfd88f162091b642363f99d7f7cfe05
Rukunuljoy <rukunuljoypub58@gmail.com>
2024-11-11 12:08:10 +0600

    route updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect, useState } from 'react';
 import About from "@/sections/About";
 import Contact from "@/sections/Contact";
 import Footer from "@/sections/Footer";
@@ -8,17 +9,26 @@ import Projects from "@/sections/Projects";
 import Tape from "@/sections/Tape";
 import Testimonials from "@/sections/Testimonials";
 
-
 export default function App() {
-  
-  return <div> 
-    <Header /> 
-    <Hero/>
-    <Projects/>
-    <Tape/>
-    <Testimonials/>
-    <About/>
-    <Contact/>
-    <Footer/>
-    </div>;
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    // Set mounted state to true once the component is loaded on the client
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Projects />
+      <Tape />
+      <Testimonials />
+      <About />
+      <Contact />
+      <Footer />
+    </div>
+  );
 }
